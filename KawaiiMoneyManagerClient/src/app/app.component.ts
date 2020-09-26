e15d066a02237f81c4254528502e31f0baa3aa7b
Matthias Baeuml <mbaeuml@bethel.jw.org>
2020-09-26 20:48:22 +0200

    Some initial values set

--- KawaiiMoneyManagerClient/src/app/app.component.ts
+++ KawaiiMoneyManagerClient/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'KawaiiMoneyManagerClient';
+  title = 'Kawaii Money Manager';
 }
