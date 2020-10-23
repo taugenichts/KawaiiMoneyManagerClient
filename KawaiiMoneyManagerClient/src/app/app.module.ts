9ebd253d42608a03467286eb06565a9a7c4d53c1
Matthias Baeuml <mbaeuml@bethel.jw.org>
2020-10-23 21:41:12 +0200

    Basic setup of some componenets. Update of packages

--- KawaiiMoneyManagerClient/src/app/app.module.ts
+++ KawaiiMoneyManagerClient/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TitleBarComponent } from './titlebar/title-bar/title-bar.component';
+import { NavigationComponent } from './titlebar/navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TitleBarComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
---
+++ KawaiiMoneyManagerClient/src/app/titlebar/title-bar/title-bar.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-title-bar',
+  templateUrl: './title-bar.component.html',
+  styleUrls: ['./title-bar.component.scss']
+})
+export class TitleBarComponent implements OnInit {
+
+  @Input() title;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
