b4bb65ff891da7fe9e55879779461dcae64253ed
Matthias Baeuml <mbaeuml@bethel.jw.org>
2020-11-07 22:28:44 +0100

    pushed home page bit further. added media queries in scss

--- KawaiiMoneyManagerClient/src/app/content/home/dashboard-item/dashboard-item.component.ts
+++ KawaiiMoneyManagerClient/src/app/content/home/dashboard-item/dashboard-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard-item',
@@ -7,9 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardItemComponent implements OnInit {
 
+  @Input() icon: string;
+
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
